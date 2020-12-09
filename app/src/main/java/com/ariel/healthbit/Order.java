2cd08acfb7c890fde0fe54280186f15d914b5d4e
Danielgimp <danielgimpelman@gmail.com>
2020-12-09 18:10:46 +0200

    First Upload

---
+++ app/src/main/java/com/ariel/healthbit/Order.java
@@ -0,0 +1,95 @@
+package com.ariel.healthbit;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Order {
+    String OrderUID;
+    Map<String, Integer> itemQuantity;
+    double totalPrice;
+    storeProduct product;
+
+    public Order(String ID){
+       this.OrderUID=ID;
+       this.itemQuantity=new HashMap<>();
+       this.totalPrice=0;
+    }
+
+    public storeProduct getProduct() {
+        return product;
+    }
+
+    public void setProduct(storeProduct product) {
+        this.product = product;
+    }
+
+    public void setOrderUID(String orderUID) {
+        OrderUID = orderUID;
+    }
+
+    public void FillProductInMap(String name)
+    {
+        if(!this.itemQuantity.containsKey(name))
+        {
+            itemQuantity.put(name,1);
+        }
+        else
+        {
+            itemQuantity.put(name,itemQuantity.get(name)+1);
+        }
+    }
+
+    public int getItemQuantity(String productName) {
+        for(Map.Entry<String,Integer> entry : itemQuantity.entrySet())
+        {
+            if(entry.getKey()==productName)
+            {
+                return entry.getValue();
+            }
+
+        }
+        return 0;
+    }
+
+    public double getTotal(){
+        for (Map.Entry<String,Integer> entry : itemQuantity.entrySet())
+        {
+            DatabaseReference productsRefernce= FirebaseDatabase.getInstance().getReference("products");
+            ValueEventListener postListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Map<String, storeProduct> td = (Map<String, storeProduct>) dataSnapshot.getValue();
+                    for (Map.Entry<String, storeProduct> innerEntry : td.entrySet()) {
+                        HashMap nana=innerEntry.getValue();
+                        String name= (String) nana.get("name");
+                        Long Lprice=(Long) nana.get("price");
+                        double price=Lprice.doubleValue();
+                        if(entry.getKey()==name)
+                        {
+                            totalPrice+=entry.getValue()*price;
+                        }
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError)
+                {
+
+                }
+            };
+            productsRefernce.addValueEventListener(postListener);
+        }
+
+
+        return totalPrice;
+    }
+
+
+
+
+}
--- app/src/main/java/com/ariel/healthbit/store.java
+++ app/src/main/java/com/ariel/healthbit/store.java
@@ -4,14 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.util.Log;
+
+import com.firebase.client.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,8 +25,13 @@ import java.util.Map.Entry;
 
 public class store extends AppCompatActivity {
 
-    storeProduct product1,product2,product3,product4,product5,product6;
-
+    storeProduct product1,product2,product3;
+    Button gotoCart;
+    ArrayList<String> productUID=new ArrayList<String>();
+    Order currOrder =new Order("1");
+    FirebaseAuth ref=FirebaseAuth.getInstance();
+    DatabaseReference refUser,refOrders;
+    FirebaseAuth fb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,23 +40,24 @@ public class store extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //add all products from "Store" on Firebase to an array list
-        DatabaseReference productsRefernce= FirebaseDatabase.getInstance().getReference("Store");
+        DatabaseReference productsRefernce= FirebaseDatabase.getInstance().getReference("products");
         ValueEventListener postListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Map <String,storeProduct> td= (Map <String,storeProduct>) dataSnapshot.getValue();
-                ArrayList<storeProduct> data=new ArrayList<storeProduct>();
+                ArrayList<storeProduct>data=new ArrayList<storeProduct>();
                 storeProduct product;
-                for (Entry<String, storeProduct> entry : td.entrySet()) {
+                for (Entry <String, storeProduct> entry : td.entrySet()) {
                     HashMap nana=entry.getValue();
-                    Long LunitsInStock= (Long) nana.get("unitsInStock");
+                    Long LunitsInStock= (Long) nana.get("inStock");
                     int nitsInStock=LunitsInStock.intValue();
-                    Long Lkcal=(Long) nana.get("kcal");
+                    Long Lkcal=(Long) nana.get("Kcal");
                     int kcal=Lkcal.intValue();
                     Long Lprice=(Long) nana.get("price");
                     double price=Lprice.doubleValue();
                     String name= (String) nana.get("name");
-                    String subType= (String) nana.get("subType");
+                    String subType= (String) nana.get("description");
+                    productUID.add((String) entry.getKey());
                     product=new storeProduct(name,kcal,price,subType);
                     data.add(product);
 
@@ -59,10 +65,11 @@ public class store extends AppCompatActivity {
                 int productsAmount=data.size();
                 final LinearLayout lm = (LinearLayout) findViewById(R.id.linearLayout);
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                params.setMargins(0, 0, 0, 50);
+                ArrayList<Button> buttons = new ArrayList<Button>();
                 for(int i=0;i<productsAmount;i++)
                 {
-
                     LinearLayout ll = new LinearLayout(getBaseContext());
                     ll.setOrientation(LinearLayout.VERTICAL);
 
@@ -80,36 +87,56 @@ public class store extends AppCompatActivity {
 
                     TextView subType = new TextView(getBaseContext());
                     subType.setText("Description: "+data.get(i).getSubType());
-                    ll.addView(subType);
+                    ll.addView(subType,params);
 
                     // Create Button
                     final Button btn = new Button(getBaseContext());
                     // Give button an ID
-                    btn.setId(i+1);
+                    btn.setId(i);
                     btn.setText("Add To Cart");
                     // set the layoutParams on the button
+
                     btn.setLayoutParams(params);
                     //Add button to LinearLayout
                     ll.addView(btn);
                     //Add button to LinearLayout defined in XML
-                    lm.addView(ll);
-                }
 
+                    lm.addView(ll);
+                    buttons.add(btn);
+                    buttons.get(i).setOnClickListener(handleOnClick(buttons.get(i),data,currOrder));
+                }
 
             }
             @Override
             public void onCancelled(DatabaseError databaseError)
             {
+
             }
         };
         productsRefernce.addValueEventListener(postListener);
+        gotoCart=(Button) findViewById(R.id.btnCart);
+        setOnClick(gotoCart,currOrder);
 
     }
+    View.OnClickListener handleOnClick(final Button currBtn,ArrayList<storeProduct>data,Order currOrder) {
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                String productName=data.get(currBtn.getId()).getName();
+                currOrder.setOrderUID(productName);
+                currOrder.FillProductInMap(productName);
+                int OrdersNumber=currOrder.getItemQuantity(productName);
+                currBtn.setText("Add To Cart"+ "(" +OrdersNumber+ ")");
+            }
+        };
+    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
+    private void setOnClick(final Button btn, final Order currOrder){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refOrders= FirebaseDatabase.getInstance().getReference("users").child(fb.getInstance().getUid()).child("Orders");
+                refOrders.child(fb.getInstance().getUid()).setValue(currOrder.itemQuantity);
+            }
+        });
     }
 }
